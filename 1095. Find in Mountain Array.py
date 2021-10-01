ccf23035f021e6bace9e024ee86a39562501ec3f
Umair Karel <76861978+umairkarel@users.noreply.github.com>
2021-10-01 10:13:19 +0530

    Update 1095. Find in Mountain Array.py

--- 1095. Find in Mountain Array.py
+++ 1095. Find in Mountain Array.py
@@ -1,3 +1,11 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
         end = self.peak_index(mountain_arr)
