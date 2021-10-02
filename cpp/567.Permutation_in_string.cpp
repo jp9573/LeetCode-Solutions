fd07617e7383945c04ddf101a22ca92a767fd466
Mikeflames <19uec076@lnmiit.ac.in>
2021-10-03 01:07:16 +0530

    Solution to 2 Leetcode Problems

---
+++ cpp/567.Permutation_in_string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+       int map[26] = {0};
+        for(char c : s1) map[c-'a']++;
+        int i = 0;
+        int j =0 ,count_chars = s1.size();
+        while(j < s2.size()){
+            if(map[s2.at(j++) - 'a']-- > 0)
+                count_chars--;
+            if(count_chars == 0) return true;
+            if(j - i == s1.size() && map[s2.at(i++) - 'a']++ >= 0)
+                count_chars++;
+        }
+        return false;
+    }
+};
