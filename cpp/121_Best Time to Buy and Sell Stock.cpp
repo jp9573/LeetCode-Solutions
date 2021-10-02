1812774771f4658bb6f066f66624f21a481fd919
Swapna Devi <65804603+Swapna804@users.noreply.github.com>
2021-10-02 07:53:47 +0530

    added code

---
+++ cpp/121_Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,13 @@
+//Best time to buy and sell stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0;
+        int minPrice=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            minPrice=min(minPrice,prices[i]);
+            maxProfit=max(maxProfit,prices[i]-minPrice);
+        }
+        return maxProfit;
+    }
+};
