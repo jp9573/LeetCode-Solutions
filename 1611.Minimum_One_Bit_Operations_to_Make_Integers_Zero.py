30a04ee8ca404187291a66c58df85d5fd318c977
Jay Patel <jy680475@dal.ca>
2021-10-02 11:47:14 -0300

    Group problems

--- 1611.Minimum_One_Bit_Operations_to_Make_Integers_Zero.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def minimumOneBitOperations(self, n: int) -> int:
-        b = list(bin(n)[2:])
-        temp = len(b)
-        for i in range(1, temp):
-            b[i] = str(int(b[i]) ^ int(b[i-1]))
-        return int(''.join(b), 2)
