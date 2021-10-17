9d9ea4d049460d240820fea380424158c668ee67
sushantvaidkar <70023679+sushantvaidkar@users.noreply.github.com>
2021-10-17 22:38:12 +0530

    Added solution for 215_Kth_Largest_Element_in_an_Array

---
+++ cpp/215_Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        priority_queue<int>pq;
+        
+        for(auto x:nums)
+            pq.push(x);
+        
+        k--;
+        
+        
+        while(k--)
+            pq.pop();
+        
+        
+        return pq.top();
+        
+        
+        
+    }
+};
