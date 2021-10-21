a5d7b7ba7267e1249a96f2711f6b1430e6402c9f
SharanyaManusani <91478139+SharanyaManusani@users.noreply.github.com>
2021-10-21 20:07:01 +0530

    Hollow_Diamond

---
+++ py/hollow_diamond.py
@@ -0,0 +1,23 @@
+row = int(input('Enter number of row: '))
+
+# Upper part of hollow diamond
+for i in range(1, row+1):
+    for j in range(1,row-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        if j==1 or j==2*i-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+# Lower part of hollow diamond
+for i in range(row-1,0, -1):
+    for j in range(1,row-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        if j==1 or j==2*i-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
