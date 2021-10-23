ec1539f6ff4f986835a3030f09d63ebb50ca7669
Pratheek Rao <31801892+prpratheek@users.noreply.github.com>
2021-10-23 18:39:45 +0530

    Create 1313. Decompress Run-Length Encoded List.java

---
+++ 1313. Decompress Run-Length Encoded List.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i += 2) {
+            for (int j = 0; j < nums[i]; j++) {
+                list.add(nums[i + 1]);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
